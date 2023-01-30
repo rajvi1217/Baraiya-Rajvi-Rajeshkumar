a9bd461e84fdd0bf417ca3c969b2e2c2b38267da
Baraiya Rajvi Rajeshkumar <103133808+rajvi1217@users.noreply.github.com>
2023-01-30 11:44:14 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import {HttpClientModule} from  '@angular/common/http';
+import { NgxSpinnerModule } from "ngx-spinner";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
